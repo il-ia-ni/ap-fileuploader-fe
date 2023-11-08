84eee64e6b8dfb42a190c77ef8f17d31162ba426
Ilia Nikolaenko <aili70@duck.com>
2023-11-08 07:43:39 +0100

    Upd project structure

--- FileUploaderFrontend/src/app/app-routing.module.ts
+++ FileUploaderFrontend/src/app/app-routing.module.ts
@@ -1,7 +1,49 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './Guards/auth.guard';
+import { LoginComponent } from './components/login/login.component';
+import { StartComponent } from './components/start/start.component';
+import { SyncConfigComponent } from './Components/sync-config/sync-config.component';
+import { SyncPreviewComponent } from './Components/sync-preview/sync-preview.component';
+import { SyncCompleteComponent } from './Components/sync-complete/sync-complete.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'start',
+    component: StartComponent,
+    canActivate: [AuthGuard]
+  },
+  {
+    path: 'syncconfig',
+    component: SyncConfigComponent,
+    canActivate: [AuthGuard]
+  },
+  {
+    path: 'syncpreview',
+    component: SyncPreviewComponent,
+    canActivate: [AuthGuard]
+  },
+  {
+    path: 'synccomplete',
+    component: SyncCompleteComponent,
+    canActivate: [AuthGuard]
+  },
+  // For nesting routes within a components root see https://angular.io/guide/router#nesting-routes
+  {
+    path: '',
+    redirectTo: 'start',
+    pathMatch: 'full',
+  },
+  {
+    path: '**',
+    component: StartComponent,  // TODO: Develop a 404-component https://angular.io/guide/router#displaying-a-404-page
+    canActivate: [AuthGuard]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- FileUploaderFrontend/src/app/app.module.ts
+++ FileUploaderFrontend/src/app/app.module.ts
@@ -1,18 +1,65 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import { NgxSpinnerModule } from "ngx-spinner";
+
+import { MatTableModule } from '@angular/material/table';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import {MatSelectModule} from '@angular/material/select';
+import {MAT_SNACK_BAR_DEFAULT_OPTIONS, MatSnackBarModule} from '@angular/material/snack-bar';
+
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { LoginComponent } from './components/login/login.component';
+import { StartComponent } from './components/start/start.component';
+import { TokenInterceptorService } from './Interceptors/http.jwt.interceptor';
+import { SyncConfigComponent } from './Components/sync-config/sync-config.component';
+import { SyncPreviewComponent } from './Components/sync-preview/sync-preview.component';
+import { SyncCompleteComponent } from './Components/sync-complete/sync-complete.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    LoginComponent,
+    StartComponent,
+    SyncConfigComponent,
+    SyncPreviewComponent,
+    SyncCompleteComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    NgxSpinnerModule.forRoot({ type: 'ball-scale-multiple' }),  // See https://napster2210.github.io/ngx-spinner/
+    MatTableModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatSelectModule,
+    MatSnackBarModule
   ],
-  providers: [],
+  providers: [
+    {provide: HTTP_INTERCEPTORS, useClass: TokenInterceptorService, multi: true},
+    {provide: MAT_SNACK_BAR_DEFAULT_OPTIONS, useValue: {
+      announcementMessage: "Empty notification",
+      horizontalPosition: "center",
+      verticalPosition: "top",
+    }}, // See https://material.angular.io/components/snack-bar/api#MatSnackBarConfig
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
