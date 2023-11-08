c5289ac8adbcbcee5ea5f829138f6715cb8ccab4
Ilia Nikolaenko <aili70@duck.com>
2023-11-08 07:54:00 +0100

    Set up a central AuthGuard for project Roots

---
+++ FileUploaderFrontend/src/app/Guards/auth.guard.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  Router,
+  RouterStateSnapshot,
+  UrlTree,
+} from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from '../Services/auth.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthGuard implements CanActivate {
+  constructor(
+    private router: Router,
+    private authService: AuthService,
+    private _snackBar: MatSnackBar
+  ) { }
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ):
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
+    if (!this.authService.isAuthenticated() || this.authService.isTokenExpired()) {
+      this.router.navigate(['login']);
+      this.authService.logout();
+      return false;
+    }
+
+    this.authService.restoreAuthentication();
+
+    return true;
+  }
+}
